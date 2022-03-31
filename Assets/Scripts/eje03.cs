57f0bd5aef2e7a7e25b81daec3997b3b0b353475
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-03-30 23:25:33 -0300

    ej01 al ej05
    ej05 no funca (rari)

---
+++ Assets/Scripts/eje03.cs
@@ -0,0 +1,22 @@
+﻿//Crear una variable pública que permita ingresar texto. Permitir que al ejecutar el programa
+//se muestre en la consola un mensaje de saludo que incluya el texto de la variable. Por
+//ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola
+//Ana”.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eje03 : MonoBehaviour
+{
+   public string input;
+    void Start()
+    {
+        Debug.Log("Hola " + input);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

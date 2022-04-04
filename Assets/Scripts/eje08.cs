e656b6fe4ee4ba1664d9d84ebf7fbe34b810590d
CarolaOrbuch <101721194+CarolaOrbuch@users.noreply.github.com>
2022-04-04 14:35:27 -0300

    ej 08
    terminado pero no se si pushee bien

---
+++ Assets/Scripts/eje08.cs
@@ -0,0 +1,49 @@
+﻿//Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+//el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+//valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+//mensaje de error "El día ingresado no es válido".
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eje08 : MonoBehaviour
+{
+    public int numDia;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (numDia)
+        {
+            case 1:
+                Debug.Log("domingo");
+                break;
+            case 2:
+                Debug.Log("lunes");
+                break;
+            case 3:
+                Debug.Log("martes");
+                break;
+            case 4:
+                Debug.Log("miercoles");
+                break;
+            case 5:
+                Debug.Log("jueves");
+                break;
+            case 6:
+                Debug.Log("viernes");
+                break;
+            case 7:
+                Debug.Log("sabado");
+                break;
+            default:
+                Debug.Log("el dia ingresado no es valido");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
